23ad612e5879a2fddebc9cc836fb1e8f7645e309
DLTK - Deep Learning Toolkit <connect@dltk.ai>
2020-08-05 13:56:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dltkai",
-    version="1.0.4",
+    version="1.0.6",
     author="DLTK",
     author_email="connect@qubitai.tech",
     description="Python Client for DLTK.",
